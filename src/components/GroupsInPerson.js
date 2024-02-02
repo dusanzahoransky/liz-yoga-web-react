48a1982a2585d0f967f37ec3a8f39b603be6ae75
dusan <dusan@rezdy.com>
2024-02-02 21:19:37 +1100

    update content

--- src/components/GroupsInPerson.js
+++ src/components/GroupsInPerson.js
@@ -86,7 +86,7 @@ function GroupsInPerson() {
 
                         <p className="text-justify">Sitio de encuentro: Dolphins Point. Llegas a Coogee Pavilion y
                             caminas unos 200 metros hacia
-                            arriba de la colina.</p>
+                            arriba de la colina. (haz click el mapa para que te muestre el lugar exacto de encuentro).</p>
                         <p>Si en algún caso cambiamos la locación, será justo a unos pocos metros de este lugar, sigues
                             caminando hacia la izquierda y nos encontrarás.</p>
 
@@ -99,14 +99,15 @@ function GroupsInPerson() {
                             </a>
                         </div>
                         <p>Hora: 6:40 pm</p>
-                        <p>Lugar: Centennial Park entrada del TAFE</p>
+                        <p>Lugar: Centennial Park en la entrada Randwick Gates - Entrada donde esta el Tafe.
+                            (haz click el mapa para que te muestre el lugar exacto de encuentro). </p>
 
 
                         <h3>Valor</h3>
                         <p>$23 AUD.</p>
-                        <p className="text-justify">Realiza el pago de la clase al número celular 0413147237 para
+                        <p className="text-justify">Realiza la transferencia de la clase al número celular 0413147237 para
                             separar tu espacio, se reciben
-                            confirmaciones hasta el dia Jueves.</p>
+                            confirmaciones y pago para la clase entre semana al atardecer hasta el Martes 6 pm y la clase de la playa hasta el viernes a las 2 pm.</p>
                         <PreparationItems preparationItems={[
                             'Ser puntuales es muy importante, la clase inicia a la hora programada, asi que, te recomiendo puntualidad para que no te pierdas ninguna parte de la clase.',
                             'Luego de confirmada tu asistencia no se hace devolución del dinero.',
