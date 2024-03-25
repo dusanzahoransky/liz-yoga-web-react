4ca2c46f1e865bc87086b20a2c847cb9cff93374
Liz Neira <lizneirar@gmail.com>
2024-03-25 15:36:01 +1100

    update content

--- src/components/RetreatBaliSept2024.js
+++ src/components/RetreatBaliSept2024.js
@@ -62,9 +62,9 @@ function RetreatBaliSept2024() {
         } else {
             return <>
                 <div className="row space-before">
-                    <p><i className="fa fa-tag"></i> <span className="fw-bolder">EARLY BIRD</span> discount <span
-                        className="fw-bolder">$100</span> descuento antes del 30 de Marzo (Acordamos cuotas para el
-                        resto del pago). Pago Total del Retiro hasta el día 04 de Julio.</p>
+                    {/* <p><i className="fa fa-tag"></i> <span className="fw-bolder">EARLY BIRD</span> discount <span
+                        className="fw-bolder">$100</span> descuento antes del 30 de Marzo Acordamos cuotas para el */}
+                        
                     {/*<p><i className="fa fa-tags"></i> <span className="fw-bolder">INVITA A UN AMIGA(O)</span> descuento*/}
                     {/*    solo para ti de <span*/}
                     {/*        className="fw-bolder">$100</span> por recomendar a un amiga(o) y realize la reservación del*/}
@@ -72,7 +72,7 @@ function RetreatBaliSept2024() {
                     <p><i className="fa fa-credit-card"></i> <span
                         className="fw-bolder">RESERVA TU ESPACIO:</span> Deposito
                         (no reembolsable) de $500 Aud con transferencia
-                        electrónica al numero celular 0413147237. </p>
+                        electrónica al numero celular 0413147237. El resto del pago Pago Total del Retiro hasta el día 04 de Julio. </p>
                 </div>
             </>
         }
