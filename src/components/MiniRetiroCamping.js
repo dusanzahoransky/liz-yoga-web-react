58d2e9b6ea6679e79f3b3b812adf2db0dea3ff5c
Liz Neira <lizneirar@gmail.com>
2025-02-10 11:38:32 +1100

    content update

--- src/components/MiniRetiroCamping.js
+++ src/components/MiniRetiroCamping.js
@@ -43,7 +43,7 @@ function MiniRetiroCamping() {
 
     function isPrivate() {
         const queryParams = new URLSearchParams(window.location.search);
-        return queryParams && queryParams.get('code') === 'MiniRetiroFebrero'
+        return queryParams && queryParams.get('code') === 'MiniRetiroyogaaventura'
     }
 
     function getPriceSection() {
@@ -52,12 +52,12 @@ function MiniRetiroCamping() {
             </>
         } else {
             return <>
-                <h3>Precio</h3>
-                <p>Intercambio de Valor: 99 AUD
-                    <ul>
-                        <li>CUPOS LIMITADOS</li>
-                    </ul>
-                </p>
+                <h3>Intercambio de Valor</h3>
+                <p> 111 AUD, Plazo para Reservar tu Particiáción hasta el <b>Martes 18 de Febrero.</b></p>
+
+                <p><b>CUPOS LIMITADOS</b></p>
+
+
                 <p>Separa tu espacio realizando transferencia al número celular 0413147237.</p>
             </>
         }
@@ -67,7 +67,7 @@ function MiniRetiroCamping() {
 
         <div className="cover-shot-container">
             <div className="cover-shot-caption cover-shot-caption-light">
-                <h1 className="display-3">Ananda, Retiro de Yoga y Bienestar</h1>
+                <h1 className="display-3">MiniRetiro de Yoga y Aventura</h1>
 
             </div>
             <picture>
@@ -82,41 +82,78 @@ function MiniRetiroCamping() {
 
                 <div className="row">
                     <div className="col-md-8">
-                        <p className="text-justify">Te invito a nuestro Primer Mini-Retiro de Inicio de Año el Domingo 09 de Febrero, en donde tendremos diferentes prácticas espirituales que nos ayudarán a seguir expandiendo nuestra conciencia. Adicionalmente es la oportunidad perfecta para seguir conectando con otros seres humanos que al igual que tú  buscan un espacio de relajación, movimiento y aprendizaje, un espacio para compartir reflexiones, experiencias y ofrecer apoyo emocional. Mujeres y Hombres bienvenidos.</p>
+                        <p className="text-justify">Te invito a nuestro Primer Mini-Retiro de Yoga y Aventura el primer fin de semana de Marzo, será un espacio exclusivo para los practicantes de mi Comunidad, un espacio para compartir y elevar la energia del grupo, tendremos diferentes actividades pensadas en el Bienestar Fisico, Mental, Espiritual, Energetico y Emocional, un MiniRetiro de Aventura para que salgamos de la rutina del dia a dia y tengamos un fin de semana de pura transformación y contacto con la naturaleza.</p>
+                        <p className="text-justify">Adicionalmente es la oportunidad perfecta para seguir conectando con otros seres humanos que al igual que tú  buscan un espacio de relajación, movimiento y aprendizaje, un espacio para compartir reflexiones, experiencias y ofrecer apoyo emocional.</p>
 
-                        <p className="text-justify"><b>Mini-Retiro muy especial de casi 4 Horas dedicadas a nutrir tu ser a través de:
+
+                        <p className="text-justify"><b>Mini-Retiro especial de fin de Semana dedicado a nutrir tu ser con:
                         </b></p>
+                        <p className="text-justify"><b>Sabado en la Tarde
+                                                </b></p>
                         <ul className="class-preparation-list text-justify">
-                            <p>Campsite facilities:	picnic tables, fire pits, barbecue facilities, drinking water, warm showers, toilets, electric power, kettle</p>
-
 
                             <li>Ejercicios de Respiración para Activar tu Energía Vital.</li>
-                            <li>Meditación Guiada con Baño de Sonido con Cuencos Tibetanos y de Cristal y otros
-                                instrumentos.
+                            <li>Meditación Guiada con Baño de Sonidos
                             </li>
-                            <li>Sesión Multinivel de Yoga enfocada a al chakra de la intuición, Ajna Chakra, Sexto centro energético de tu sistema de chakras, Este chakra está asociado con la conciencia, la comprensión y la clarividencia. Es un centro de percepción extrasensorial, percepción de la verdad y comprensión intuitiva.
-                            </li>
-                            <li>Taller de Numerología, vas a reconocer en qué año estás, cual es la energía que tienes disponible ahora mismo utilizando nuestro nombre, fecha de nacimiento y otros aspectos que te darán muchísima información para que utilices esa energía para tu más alto beneficio..
-                            </li>
+                            <li>Sesión de Vinyasa Flow. </li>
+                             <li>Camping and Hiking Masterclass by Dusan Zahoranksy</li>
+                             <li>Caminata</li>
+                             <li>Excursión al Rio</li>
+                             <li>Juegos, Conversaciones y Mucho Más</li>
+                             </ul>
 
-                            <li>Snacks Nutritivos.
+                         <p className="text-justify"><b>
+                        </b></p>
+                          <p className="text-justify"><b>Domingo en la Mañana
+                                                                          </b></p>
+                                                  <ul className="class-preparation-list text-justify">
+
+                              <li>Ejercicios de Respiración para Activar tu Energía Vital</li>
+                              <li>Meditación Guiada con Baño de Sonidos
+                              </li>
+                               <li>Vinyasa Yoga</li>
+                               <li>Caminata</li>
+                               <li>Excursión al Rio</li>
+ </ul>
+
+<p className="text-justify"><b>
+                        </b></p>
+                          <p className="text-justify"><b>Campsite Facilities
+                                                                          </b></p>
+                                                  <ul className="class-preparation-list text-justify">
+
+                            <li>
+                             <p>Mesas de picnic, fogatas, BBQs, cocina, duchas con agua caliente, baños, Tomacorrientes, kettle, microondas.</p>
                             </li>
+ <p className="text-justify"><b>
+                        </b></p>
+                          <p className="text-justify"><b>Que NO incluye el MiniRetiro
+                                                                          </b></p>
+
+                           <li>Comida, trae tus propios alimentos </li>
+                           <li>Tienda de Acampar/Carpa </li>
+                           <li>Valor del Alquiler del campamento, la idea es compartir los spots para acampar y dividir el valor entre los que compartamos el spot, nosotros nos encargaremos de hacer la reservación </li>
+                           <li>Transporte, deberas ir en carro, la idea será que salgamos en grupo y compartamos el carro entre las personas que se registren, primero para comenzar a integrarnos como grupo, segundo para ayudar con el medio ambiente y tercero para que se compartan gastos de gasolina.</li>
+
 
                         </ul>
                         <h3>Lugar</h3>
-                        <p>Coogee Beach, Goldstein Reserve - Sydney.</p>
+                        <p>Campsite localizado a 3 horas en carro, salida hacia el sur de Sydney.</p>
                         <h3>Fecha y Hora</h3>
-                        <p>Domingo 09 de Febrero, 7:15 AM a 10:30/11 AM.</p>
+                        <p>Fin de Semana del 1 al 2 de Marzo, idealmente saliendo de Sydney a las 6 am.</p>
+                        <p>El horario para hacer Check in el Sabado es a las 2 pm y Checkout 10 am, sin embargo llegaremos al lugar el sabado temprano y exploraremos el sitio y luego haremos el checkin para armar las tiendas de acampar.</p>
                         {getPriceSection()}
 
                         <span className="fw-bold">
-                            Información Importante: En caso de lluvia, cambiaremos el lugar (Indoor) y probablemente la hora de encuentro, te informaré el Martes de esa misma semana, así que te sugiero reservar el domingo hasta las 3 pm solo para ti.
+                            Información Importante: En caso de lluvia, se aplazará el MiniRetiro para el fin de semana del 8 y 9 de Marzo,es por ello que es indispensable reserves ese fin de semana tambien, el Martes 24 de Febrero sabremos el clima y decidiremos, sin embargo estamos convencidos que contaremos con un muy buen clima.
                         </span>
                         <PreparationItems preparationItems={[
+                          'Será un espacio exclusivo para mi comunidad, así que el requisito indispensable es que te diviertas, descanses y sonrias.',
+                          'Lleva tu Mat y Props de Yoga.',
+                          'Es importante que tengas un tarrito de agua para que te mantegas hidratado/da.',
                             'Debes realizar el pago por anticipado para reservar tu espacio.',
-                            'Ser puntuales es muy importante, nuestro Sesión inicia a la hora programada, asi que, te recomiendo puntualidad.',
                             'Luego de confirmada tu asistencia no se realiza la devolución del dinero, en caso de no poder asistir puedes ceder tu espacio a alguien más.',
-                            'Es importante que tengas un tarrito de agua para que te mantegas hidratado/da.'
+
                         ]}/>
                     </div>
                     <div className="col-md-4">
